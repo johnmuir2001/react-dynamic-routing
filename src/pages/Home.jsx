d2b99ea8a86eee11c36d698bb11f8e21d209b52d
John Muir <57762234+johnmuir2001@users.noreply.github.com>
2024-10-01 11:05:29 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ const Home = () => {
     const films = ["Coco", "Finding-Nemo", "Toy-Story"]
 
     return (
-        <>
+        <div className="allFilms">
             {/* Use the link to navigate to the page using the films ID as the destination url */}
             {films.map((film, i) => {
                 return <Link key={i} to={`/${film}`} >{film}</Link>
             })}
-        </>
+        </div>
     )
 }
 
