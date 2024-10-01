26b3c580a155bde8a1de77e5f07d590b9a565c1a
John Muir <57762234+johnmuir2001@users.noreply.github.com>
2024-10-01 11:02:13 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [films, setFilms] = useState([])
-
-    useEffect(() => {
-        const fetchAPI = async () => {
-            const res = await fetch("https://ghibliapi.vercel.app/films")
-            const data = await res.json();
-            setFilms(data)
-        }
-        fetchAPI()
-    }, [])
+    const films = ["Coco", "Finding-Nemo", "Toy-Story"]
 
     return (
         <>
-            {films.length === 0 && <h1>Loading...</h1>}
             {/* Use the link to navigate to the page using the films ID as the destination url */}
             {films.map((film, i) => {
-                return <Link key={i} to={`/${film.id}`} >{film.title}</Link>
+                return <Link key={i} to={`/${film}`} >{film}</Link>
             })}
         </>
     )
